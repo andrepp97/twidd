5b84738bb5e9170fd460b75825155312f7b4abaf
Andre Putera Pratama <andreputerap@gmail.com>
2022-06-27 03:04:45 +0700

    Update main.tsx

--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -1,20 +1,25 @@
+import { useSession } from "next-auth/react";
 import Sidebar from "./sidebar";
 import Widgets from "./widgets";
 
 interface LayoutProps {
-    children: React.ReactNode;
+    children: React.ReactNode
 }
 
 const Main = ({ children }: LayoutProps) => {
+    // Hooks
+    const { data: session } = useSession()
+
+    // Render
     return (
         <main className='lg:max-w-7xl mx-auto'>
             <div className="flex">
-                <Sidebar />
+                {session && <Sidebar />}
                 <main className='grid grid-cols-6 w-full max-h-screen overflow-y-hidden'>
-                    <div className='col-span-6 lg:col-span-4 overflow-auto px-2'>
+                    <div className={`col-span-6 ${session && "lg:col-span-4"} overflow-auto`}>
                         {children}
                     </div>
-                    <Widgets />
+                    {session && <Widgets />}
                 </main>
             </div>
         </main>
