44690b5e239122a5150a6d0d72a515c8881eeb2c
Andre Putera Pratama <andreputerap@gmail.com>
2022-06-27 03:04:23 +0700

    Create userProfile.tsx

---
+++ src/components/userProfile.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image"
+import { DotsHorizontalIcon } from "@heroicons/react/outline"
+
+interface ProfileProps {
+    open: boolean
+    session: any
+    signout: Function
+    openMenu: Function
+}
+
+const UserProfile = ({ open, openMenu, session, signout }: ProfileProps) => {
+    return (
+        <div className='relative'>
+            <div className={`absolute top-[-4rem] bg-embed w-full h-fit p-2 rounded shadow shadow-zinc-500 transition-all duration-200 opacity-0 ${open && "opacity-100"}`}>
+                <button
+                    onClick={() => signout()}
+                    className='w-full tracking-wide text-zinc-100 hover:bg-gray-700 rounded p-2'
+                >
+                    Logout <small className='text-gray-300'>@{session?.user?.tag}</small>
+                </button>
+            </div>
+            <div
+                onClick={() => openMenu()}
+                className='flex items-center rounded-full cursor-pointer w-fit p-2 mx-auto hover:bg-embed'
+            >
+                <Image
+                    src={session?.user?.image || ""}
+                    className='rounded-full'
+                    height={42}
+                    width={42}
+                    alt=""
+                />
+                <div className='hidden md:inline leading-5 mr-4 ml-2'>
+                    <h4 className='font-semibold text-zinc-300'>
+                        {session?.user?.name}
+                    </h4>
+                    <p className='text-gray-400 text-xs'>
+                        @{session?.user?.tag}
+                    </p>
+                </div>
+                <DotsHorizontalIcon
+                    className='h-5 hidden md:inline text-gray-300'
+                />
+            </div>
+        </div>
+    );
+}
+
+export default UserProfile;
