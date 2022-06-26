2c1435924edd12caf7dcf890ef16f2bc274f76dd
Andre Putera Pratama <andreputerap@gmail.com>
2022-06-27 03:04:43 +0700

    Update tweetBox.tsx

--- src/components/tweetBox.tsx
+++ src/components/tweetBox.tsx
@@ -1,53 +1,175 @@
 import Image from 'next/image'
-import User from '../../public/user.png'
+import { useRef, useState } from 'react'
+import { useSession } from 'next-auth/react'
 import {
     PhotographIcon,
     EmojiHappyIcon,
     CalendarIcon,
+    XIcon,
 } from '@heroicons/react/outline'
-import { useState } from 'react'
+import {
+    doc,
+    addDoc,
+    updateDoc,
+    collection,
+    serverTimestamp,
+} from "@firebase/firestore"
+import {
+    ref,
+    uploadString,
+    getDownloadURL,
+} from "@firebase/storage"
+import Spinner from './spinner'
+import { db, storage } from "../lib/firebase"
+import { Picker } from "emoji-mart"
+import "emoji-mart/css/emoji-mart.css"
 
 const TweetBox = () => {
-    // State
+    // Hooks
+    const { data: session }: any = useSession()
+    const filePickerRef = useRef<HTMLInputElement>(null)
     const [tweet, setTweet] = useState<string>("")
+    const [image, setImage] = useState<any>(null)
+    const [loading, setLoading] = useState<boolean>(false)
+    const [showEmoji, setShowEmoji] = useState<boolean>(false)
+
+    // Function
+    const pickImage = () => filePickerRef.current && filePickerRef.current.click()
+
+    const addImage = (e: { target: { files: any } }) => {
+        const reader = new FileReader()
+        if (e.target.files[0]) reader.readAsDataURL(e.target.files[0])
+        reader.onload = (readerEvent) => setImage(readerEvent.target?.result)
+    }
+
+    const onEmojiClick = (e: any) => {
+        let symbol = e.unified.split("-")
+        let codesArray: any[] = []
+        symbol.forEach((char: string) => codesArray.push("0x" + char))
+        let emoji = String.fromCodePoint(...codesArray)
+        setTweet(tweet + emoji)
+    }
+
+    const sendTweet = async () => {
+        try {
+            setLoading(true)
+
+            const docRef = await addDoc(collection(db, "posts"), {
+                id: session.user.uid,
+                username: session.user.name,
+                userImg: session.user.image,
+                tag: session.user.tag,
+                text: tweet,
+                timestamp: serverTimestamp(),
+            })
+
+            const imageRef = ref(storage, `posts/${docRef.id}/image`)
+
+            if (image) {
+                await uploadString(imageRef, image, "data_url")
+                    .then(async () => {
+                        const downloadURL = await getDownloadURL(imageRef)
+                        await updateDoc(doc(db, "posts", docRef.id), { image: downloadURL })
+                    })
+                    .catch(err => console.log(err))
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+            setShowEmoji(false)
+            setImage(null)
+            setTweet("")
+        }
+    }
 
     // Render
     return (
-        <div className="flex space-x-4 p-4">
+        <div className={`flex border-b border-gray-700 space-x-2 p-4 ${loading && "opacity-75"}`}>
 
             <div className="w-fit">
                 <Image
+                    src={session.user.image}
                     className="rounded-full"
                     objectFit="contain"
-                    height={60}
-                    width={60}
-                    src={User}
-                    alt="user"
+                    height={48}
+                    width={48}
+                    alt="You"
                 />
             </div>
 
             <div className="flex flex-1 items-center">
-                <div className="flex flex-1 flex-col">
+                <div className="flex flex-1 flex-col space-y-2">
                     <textarea
                         rows={3}
                         value={tweet}
                         onChange={e => setTweet(e.target.value)}
-                        className="text-zinc-100 outline-none rounded-md border-zinc-700 border bg-transparent p-2"
+                        className="text-zinc-100 outline-none rounded-lg border-zinc-700 border bg-transparent tracking-wide min-h-[48px] p-2"
                         placeholder="What's Happening ?"
                     />
-                    <div className="flex items-center mt-2">
-                        <div className="flex flex-1 space-x-2 text-gray-300">
-                            <PhotographIcon className="h-5 w-5 cursor-pointer hover:text-zinc-100" />
-                            <EmojiHappyIcon className="h-5 w-5 cursor-pointer hover:text-zinc-100" />
-                            <CalendarIcon className="h-5 w-5 cursor-pointer hover:text-zinc-100" />
+
+                    {image && (
+                        <div className="relative">
+                            <div
+                                className="absolute w-8 h-8 bg-[#15181c] hover:bg-gray-700 bg-opacity-90 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer"
+                                onClick={() => setImage(null)}
+                            >
+                                <XIcon className="text-white h-5" />
+                            </div>
+                            <img
+                                alt=""
+                                src={image}
+                                className="rounded-lg object-contain max-h-96"
+                            />
                         </div>
-                        <button
-                            disabled={!tweet}
-                            className="rounded-full font-semibold text-gray-300 bg-embed hover:bg-zinc-700 active:bg-embed transition-all duration-150 disabled:opacity-50 disabled:hover:bg-embed px-4 py-2"
-                        >
-                            Tweet
-                        </button>
-                    </div>
+                    )}
+
+                    {
+                        loading
+                            ? <Spinner />
+                            : (
+                                <div className="flex items-center">
+                                    <div className="flex flex-1 space-x-3 text-gray-300">
+                                        <div onClick={pickImage}>
+                                            <PhotographIcon className="h-5 w-5 cursor-pointer hover:text-zinc-100" />
+                                            <input
+                                                hidden
+                                                type="file"
+                                                accept="image/*"
+                                                ref={filePickerRef}
+                                                onChange={addImage}
+                                            />
+                                        </div>
+                                        <div onClick={() => setShowEmoji(prev => !prev)}>
+                                            <EmojiHappyIcon
+                                                className="h-5 w-5 cursor-pointer hover:text-zinc-100"
+                                            />
+                                        </div>
+                                        <CalendarIcon className="h-5 w-5 cursor-pointer hover:text-zinc-100" />
+                                        {showEmoji && (
+                                            <Picker
+                                                theme="dark"
+                                                onSelect={onEmojiClick}
+                                                style={{
+                                                    position: "absolute",
+                                                    marginLeft: "-3rem",
+                                                    marginTop: "2rem",
+                                                    maxWidth: "270px",
+                                                    borderRadius: "1rem",
+                                                }}
+                                            />
+                                        )}
+                                    </div>
+                                    <button
+                                        onClick={sendTweet}
+                                        disabled={!tweet.trim()}
+                                        className="rounded-full font-semibold text-gray-300 bg-[#1D9BF0] hover:bg-[#1A8CD8] transition-all duration-150 disabled:opacity-50 px-4 py-2"
+                                    >
+                                        Tweet
+                                    </button>
+                                </div>
+                            )
+                    }
                 </div>
             </div>
 
