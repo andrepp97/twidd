c2515397a6fefdc853c600fed28175c5fdb807f5
Andre Putera Pratama <andreputerap@gmail.com>
2022-06-27 03:04:47 +0700

    Update sidebar.tsx

--- src/layout/sidebar.tsx
+++ src/layout/sidebar.tsx
@@ -1,14 +1,18 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useState } from 'react'
+import { useSession, signOut } from 'next-auth/react'
 import {
     HomeIcon,
     HashtagIcon,
     BellIcon,
     MailIcon,
     UserIcon,
+    PencilIcon,
 } from '@heroicons/react/outline'
-import SidebarItem from '../components/sidebarItem'
 import TwitterIcon from '../../public/twitter.png'
+import SidebarItem from '../components/sidebarItem'
+import UserProfile from '../components/userProfile'
 
 const sidebarItems = [
     {
@@ -39,26 +43,56 @@ const sidebarItems = [
 ]
 
 const Sidebar = () => {
+    // Hooks
+    const { data: session }: any = useSession()
+    const [open, setOpen] = useState(false)
+
+    // Function
+    const userSignout = () => {
+        signOut()
+        setOpen(false)
+    }
+
+    // Render
     return (
-        <aside className='min-h-screen border-r border-zinc-700 space-y-2 w-max px-2 md:px-6 py-6'>
-            <Link href={"/"} passHref={true}>
-            <div className='w-full flex justify-center sm:justify-start sm:ml-6 mb-6'>
-                <Image
-                    className='text-gray-300 cursor-pointer'
-                    src={TwitterIcon}
-                    height={22}
-                    width={28}
-                />
+        <aside className='min-h-screen min-w-fit flex flex-col justify-between border-r border-zinc-700 space-y-2 px-1 md:px-6 py-5'>
+
+            <div>
+                <Link href={"/"} passHref={true}>
+                    <div className='w-full flex justify-center sm:justify-start sm:ml-6 mb-6'>
+                        <Image
+                            className='text-gray-300 cursor-pointer'
+                            src={TwitterIcon}
+                            height={22}
+                            width={28}
+                        />
+                    </div>
+                </Link>
+                <div className='space-y-1 my-6'>
+                    {sidebarItems.map((item, index) => (
+                        <SidebarItem
+                            key={index}
+                            Icon={item.icon}
+                            title={item.title}
+                            path={item.path}
+                        />
+                    ))}
+                </div>
+                <button
+                    className='text-zinc-100 bg-[#1D9BF0] hover:bg-[#1A8CD8] transition-all duration-200 p-3 rounded-full font-semibold tracking-wide w-fit md:w-full flex items-center justify-center mx-auto'
+                >
+                    <p className='hidden md:inline'>Tweet</p>
+                    <PencilIcon className='w-5 h-5 md:hidden' />
+                </button>
             </div>
-            </Link>
-            {sidebarItems.map((item, index) => (
-                <SidebarItem
-                    key={index}
-                    Icon={item.icon}
-                    title={item.title}
-                    path={item.path}
-                />
-            ))}
+
+            <UserProfile
+                open={open}
+                session={session}
+                signout={userSignout}
+                openMenu={() => setOpen(prev => !prev)}
+            />
+
         </aside>
     );
 }
