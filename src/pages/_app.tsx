4f8e55965a96e4b632ca370a874747bbc1caa052
Andre Putera Pratama <andreputerap@gmail.com>
2022-06-27 03:04:52 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,21 @@
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import Head from 'next/head'
 import Main from '../layout/main'
 import '../styles/globals.css'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({ Component, pageProps: {session, ...pageProps} }: AppProps) => {
     return (
         <div className='bg-zinc-900'>
             <Head>
                 <title>Tweetme</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Main>
-                <Component {...pageProps} />
-            </Main>
+            <SessionProvider session={session}>
+                <Main>
+                    <Component {...pageProps} />
+                </Main>
+            </SessionProvider>
         </div>
     )
 }
