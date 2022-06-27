09e8e259bd979bc50f6a76500cf15957455a7eac
Andre Putera Pratama <andreputerap@gmail.com>
2022-06-27 22:08:30 +0700

    Update widgets.tsx

--- src/layout/widgets.tsx
+++ src/layout/widgets.tsx
@@ -3,7 +3,7 @@ import { TwitterTimelineEmbed } from "react-twitter-embed"
 
 const Widgets = () => {
     return (
-        <div className="border-l border-zinc-700 space-y-4 p-4 pt-4 hidden lg:inline col-span-2 overflow-auto">
+        <div className="border-l border-zinc-700 space-y-4 py-4 px-6 hidden lg:inline col-span-2 overflow-auto">
 
             {/* Search */}
             <div className="flex items-center rounded-full space-x-2 px-4 py-2 bg-embed text-gray-400">
