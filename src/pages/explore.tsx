20aec2c54e0871d0e1c3e47998d47abf26ee818e
Andre Putera Pratama <andreputerap@gmail.com>
2022-06-28 01:07:03 +0700

    Update explore.tsx

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,9 +1,12 @@
 const Explore = () => {
     return (
-        <div className="min-h-screen text-white">
-            <h1>
-                Ini Halaman Explore
+        <div className="min-h-screen text-gray-300 flex flex-col items-center justify-center gap-4 p-4">
+            <h1 className="text-2xl">
+                Under Construction
             </h1>
+            <p>
+                Please Come Back Later
+            </p>
         </div>
     );
 }
