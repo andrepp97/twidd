768aba5e9fa680dc045fc882aa30d6a9991b3f9a
Andre Putera Pratama <andreputerap@gmail.com>
2022-06-29 03:23:52 +0700

    Update sidebarItem.tsx

--- src/components/sidebarItem.tsx
+++ src/components/sidebarItem.tsx
@@ -16,7 +16,7 @@ const SidebarItem = ({ Icon, title, path }: SidebarProps) => {
     return (
         <Link href={path} passHref={true}>
             <div className={`text-gray-300 flex items-center justify-center sm:justify-start space-x-2 px-3 sm:px-6 py-3 cursor-pointer rounded-full hover:text-zinc-100 hover:bg-zinc-700 transition-all duration-200 ${pathname == path && 'bg-zinc-700 text-zinc-100 font-semibold'}`}>
-                <Icon className='w-6 h-6' />
+                <Icon className='w-5 sm:w-6 h-5 sm:h-6' />
                 <p className='hidden md:inline-flex lg:text-lg'>
                     {title}
                 </p>
