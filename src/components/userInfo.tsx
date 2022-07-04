1ad3331066167f8ffee1e606534bc695cdd812e7
Andre Putera Pratama <andreputerap@gmail.com>
2022-07-05 02:42:08 +0700

    Update userInfo.tsx

--- src/components/userInfo.tsx
+++ src/components/userInfo.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image"
-import { DotsHorizontalIcon, LogoutIcon } from "@heroicons/react/outline"
 
 interface ProfileProps {
     open: boolean
@@ -11,20 +10,17 @@ interface ProfileProps {
 const UserInfo = ({ open, openMenu, session, signout }: ProfileProps) => {
     return (
         <div className='relative'>
-            <div className={`absolute top-[-60px] bg-embed w-fit sm:w-full h-fit p-1 rounded shadow shadow-zinc-500 transition-all duration-200 opacity-0 ${open && "opacity-100"}`}>
+            <div className={`absolute -top-20 md:-top-[60px] bg-embed w-fit md:w-full h-fit p-1 rounded shadow shadow-zinc-500 transition-all duration-200 opacity-0 ${open && "opacity-100"}`}>
                 <button
                     onClick={() => signout()}
                     className='w-full tracking-wide text-zinc-100 hover:bg-gray-700 rounded p-2'
                 >
-                    <span className="hidden md:inline">
-                        Logout <small className='text-gray-300'>@{session?.user?.tag}</small>
-                    </span>
-                    <LogoutIcon className="w-6 h-6 md:hidden" />
+                    Logout <small className='text-gray-300'>@{session?.user?.tag}</small>
                 </button>
             </div>
             <div
                 onClick={() => openMenu()}
-                className='flex items-center rounded-full cursor-pointer w-fit sm:p-2 mx-auto hover:bg-embed'
+                className='flex items-center rounded-full cursor-pointer sm:p-2 mx-auto hover:bg-embed'
             >
                 <Image
                     src={session?.user?.image || ""}
@@ -33,7 +29,7 @@ const UserInfo = ({ open, openMenu, session, signout }: ProfileProps) => {
                     width={40}
                     alt=""
                 />
-                <div className='hidden md:inline leading-5 mr-4 ml-2'>
+                <div className='hidden md:inline leading-5 mx-2'>
                     <h4 className='font-semibold text-zinc-300'>
                         {session?.user?.name}
                     </h4>
@@ -41,9 +37,6 @@ const UserInfo = ({ open, openMenu, session, signout }: ProfileProps) => {
                         @{session?.user?.tag}
                     </p>
                 </div>
-                <DotsHorizontalIcon
-                    className='h-5 hidden md:inline text-gray-300'
-                />
             </div>
         </div>
     );
